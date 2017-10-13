92432d9f6207200001a1e5fb60260490fc2467bc
Yukibashiri <mariodamiaocaparroz@gmail.com>
2017-10-13 15:24:39 -0300

    Mapa fuuncional

--- app/src/main/java/com/example/mario/projetopratico/HeatMapActivity.java
+++ app/src/main/java/com/example/mario/projetopratico/HeatMapActivity.java
@@ -2,6 +2,7 @@ package com.example.mario.projetopratico;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +10,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class HeatMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,6 +37,37 @@ public class HeatMapActivity extends FragmentActivity implements OnMapReadyCallb
         mapFragment.getMapAsync(this);
     }
 
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+
+    private void addHeatMap() {
+        List<LatLng> list = null;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.focos_de_dengue);
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        TileOverlay mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +83,10 @@ public class HeatMapActivity extends FragmentActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marcador em São Luis"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng gps = new LatLng(-2.490882, -44.250214);
+        float zoomLevel = 11.0f;
+        mMap.addMarker(new MarkerOptions().position(gps).title("Marcador em São Luis"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gps,zoomLevel));
+        addHeatMap();
     }
 }
